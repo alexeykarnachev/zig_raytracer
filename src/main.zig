7bae158236eb50d1bcf1bb3ff83309f46623092f
Alexey Karnachev <alekseykarnachev@gmail.com>
2023-03-26 03:11:27 +0400

    rewrite basic routines with Vec3

--- src/main.zig
+++ src/main.zig
@@ -3,11 +3,11 @@ const math = std.math;
 
 const EPS = 1.0e-6;
 
-const WIDTH = 1000;
-const HEIGHT = 1000;
+const WIDTH = 1200;
+const HEIGHT = 600;
 const N_PIXELS = HEIGHT * WIDTH;
 
-const FOV = 60.0 * math.pi / 180.0;
+const FOV = 90.0 * math.pi / 180.0;
 const FOCAL_LEN = math.cos(FOV * 0.5) / math.sin(FOV * 0.5);
 
 const DRAW_BUFFER_SIZE = N_PIXELS * 3;
@@ -29,12 +29,12 @@ const Vec3 = struct {
         return Vec3{ .x = x, .y = y, .z = z };
     }
 
-    pub fn from_slice(s: [3]f32) Vec3 {
-        return Vec3{ .x = s[0], .y = s[1], .z = s[2] };
-    }
-
     pub fn dot(self: Vec3, other: Vec3) f32 {
         return self.x * other.x + self.y * other.y + self.z * other.z;
+    }
+
+    pub fn length(self: Vec3) f32 {
+        return @sqrt(self.x * self.x + self.y * self.y + self.z * self.z);
     }
 
     pub fn sub(self: Vec3, other: Vec3) Vec3 {
@@ -54,7 +54,11 @@ const Vec3 = struct {
     }
 
     pub fn scale(self: Vec3, k: f32) Vec3 {
-        return Vec3.init(self.x / k, self.y / k, self.z / k);
+        return Vec3.init(self.x * k, self.y * k, self.z * k);
+    }
+
+    pub fn normalize(self: Vec3) Vec3 {
+        return self.scale(1.0 / self.length());
     }
 
     pub fn print(self: Vec3) void {
@@ -106,21 +110,14 @@ pub fn fill_buffer_with_cam2pix_rays(
     const aspect: f32 = w / h;
     const pix_width: f32 = 2.0 / w;
     const pix_height: f32 = 2.0 / h;
+
     var i_pix: usize = 0;
-    var row: f32 = undefined;
-    var col: f32 = undefined;
-    var x: f32 = undefined;
-    var y: f32 = undefined;
-    var ray_len: f32 = undefined;
-
     while (i_pix < width * height) : (i_pix += 1) {
-        row = @floor(@intToFloat(f32, i_pix) / w);
-        col = @intToFloat(f32, i_pix) - w * row;
-        y = 2.0 * (h - row - 1.0) / h + 0.5 * pix_height - 1.0;
-        x = aspect * (2.0 * col / w + 0.5 * pix_width - 1.0);
-
-        ray_len = @sqrt(x * x + y * y + focal_len * focal_len);
-        buffer[i_pix] = Vec3.init(x, y, -focal_len).scale(1.0 / ray_len);
+        var row = @floor(@intToFloat(f32, i_pix) / w);
+        var col = @intToFloat(f32, i_pix) - w * row;
+        var y = 2.0 * (h - row - 1.0) / h + 0.5 * pix_height - 1.0;
+        var x = aspect * (2.0 * col / w + 0.5 * pix_width - 1.0);
+        buffer[i_pix] = Vec3.init(x, y, -focal_len).normalize();
     }
 }
 
@@ -140,23 +137,6 @@ pub fn blit_buffer_to_ppm(
     _ = try out_file.write(buffer);
 }
 
-pub fn fill_buffer_with_mango_uv_rgb(buffer: []u8, width: usize, height: usize) void {
-    const w: f32 = @intToFloat(f32, width);
-    const h: f32 = @intToFloat(f32, height);
-    var i_pix: usize = 0;
-    var row: f32 = undefined;
-    var col: f32 = undefined;
-
-    while (i_pix < width * height) : (i_pix += 1) {
-        row = @floor(@intToFloat(f32, i_pix) / w);
-        col = @intToFloat(f32, i_pix) - w * row;
-
-        buffer[i_pix * 3 + 0] = @floatToInt(u8, 255.0 * col / w);
-        buffer[i_pix * 3 + 1] = @floatToInt(u8, 255.0 - 255.0 * row / h);
-        buffer[i_pix * 3 + 2] = 0;
-    }
-}
-
 // ------------------------------------------------------------------
 // Ray intersection functions
 pub fn intersect_ray_with_sphere(
@@ -165,9 +145,14 @@ pub fn intersect_ray_with_sphere(
     center: Vec3,
     radius: f32,
 ) f32 {
-    const c = origin.sub(center);
-    const rc = ray.dot(c);
-    var d = rc * rc + radius * radius - c.dot(c);
+    const c = [3]f32{
+        origin.x - center.x,
+        origin.y - center.y,
+        origin.z - center.z,
+    };
+
+    const rc: f32 = ray.x * c[0] + ray.y * c[1] + ray.z * c[2];
+    var d: f32 = rc * rc - c[0] * c[0] - c[1] * c[1] - c[2] * c[2] + radius * radius;
 
     if (d > -EPS) {
         d = @max(d, 0);
@@ -175,7 +160,9 @@ pub fn intersect_ray_with_sphere(
         return math.nan(f32);
     }
 
-    const k = @min(-rc + @sqrt(d), -rc - @sqrt(d));
+    const k1: f32 = -rc + @sqrt(d);
+    const k2: f32 = -rc - @sqrt(d);
+    const k = @min(k1, k2);
 
     if (k < 0) {
         return math.nan(f32);
@@ -190,18 +177,6 @@ pub fn intersect_ray_with_plane(
     point: Vec3,
     normal: Vec3,
 ) f32 {
-    // const d = ray[0] * normal[0] + ray[1] * normal[1] + ray[2] * normal[2];
-
-    // if (math.fabs(d) < EPS) {
-    //     return math.nan(f32);
-    // }
-
-    // const v = [3]f32{ point[0] - origin[0], point[1] - origin[1], point[2] - origin[2] };
-    // const n = normal[0] * v[0] + normal[1] * v[1] + normal[2] * v[2];
-    // const k = n / d;
-
-    // return k;
-
     const d = ray.dot(normal);
     if (math.fabs(d) < EPS) {
         return math.nan(f32);
@@ -224,21 +199,19 @@ pub fn main() !void {
         FOCAL_LEN,
     );
 
-    // const plane: Plane = Plane.init();
-
     var origin = Vec3.init(0.0, 0.0, 0.0);
-    // var center = [3]f32{ 1.2, 0.0, -1.0 };
-    // var radius: f32 = 1.0;
+    var center = Vec3.init(0.3, 0.3, -1.0);
+    var radius: f32 = 0.3;
 
     var point = Vec3.init(0.0, -1.0, 0.0);
     var normal = Vec3.init(0.1, 1.0, 0.0);
 
-    var k: f32 = undefined;
     var i: usize = 0;
     while (i < N_PIXELS) : (i += 1) {
         var ray = CAM2PIX_RAYS[i];
-        k = intersect_ray_with_plane(ray, origin, point, normal);
-        // k = intersect_ray_with_sphere(ray, origin, center, radius);
+        var p = intersect_ray_with_plane(ray, origin, point, normal);
+        var s = intersect_ray_with_sphere(ray, origin, center, radius);
+        var k = @max(p, s);
 
         if (k > 0) {
             DRAW_BUFFER[i * 3 + 0] = 255;
