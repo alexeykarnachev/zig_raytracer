077d3c88466430d867061222bacec22f7eee8482
Alexey Karnachev <alekseykarnachev@gmail.com>
2023-03-30 01:30:42 +0400

    implement main tracing loop

--- src/main.zig
+++ src/main.zig
@@ -9,8 +9,8 @@ const SCREEN_HEIGHT: usize = 600;
 var SCREEN_BUFFER: [SCREEN_WIDTH * SCREEN_HEIGHT * 3]f32 = undefined;
 var DRAW_BUFFER: [SCREEN_WIDTH * SCREEN_HEIGHT * 3]u8 = undefined;
 
-const CAMERA_POSITION: Vec3 = Vec3.init(0.0, 0.0, -5.0);
-const CAMERA_FORWARD: Vec3 = Vec3.init(0.0, 0.0, 1.0).normalize();
+const CAMERA_POSITION: Vec3 = Vec3.init(-2.0, 2.0, 5.0);
+const CAMERA_FORWARD: Vec3 = Vec3.init(0.0, 0.4, -1.0).normalize();
 
 pub const World = struct {
     planes: []const Plane,
@@ -30,7 +30,11 @@ pub const Screen = struct {
     height: usize,
 };
 
-pub const Material = struct { albedo: Vec3 };
+pub const Material = struct {
+    albedo: Vec3 = Vec3.init(0.0, 0.0, 0.0),
+    emission: Vec3 = Vec3.init(0.0, 0.0, 0.0),
+    specular: f32 = 0.0,
+};
 
 pub const Plane = struct {
     material: Material = Material{ .albedo = Vec3.init(0.5, 0.5, 0.5) },
@@ -57,13 +61,17 @@ pub fn render_world(
     camera: Camera,
     screen: Screen,
 ) void {
+    const n_rays_per_pixel: usize = 128;
+    const max_n_ray_bounces: usize = 16;
+    const sky_material = Material{ .emission = Vec3.init(0.6, 0.5, 0.3) };
+
     const screen_width = @intToFloat(f32, screen.width);
     const screen_height = @intToFloat(f32, screen.height);
     const n_pixels = screen.width * screen.height;
     const pixel_half_width = 1.0 / (2.0 * screen_width);
     const pixel_half_height = 1.0 / (2.0 * screen_height);
     const aspect_ratio = screen_height / screen_width;
-    const camera_side = camera.up.cross(camera.forward).normalize();
+    const camera_side = camera.forward.cross(camera.up).normalize();
     const camera_focal_len = math.cos(camera.fov * 0.5) / math.sin(camera.fov * 0.5);
     const screen_center = camera.position.add(camera.forward.with_length(camera_focal_len));
 
@@ -76,59 +84,88 @@ pub fn render_world(
         var pixel_screen_y = pixel_half_height + (2.0 - 2.0 * pixel_row / screen_height) - 1.0;
         pixel_screen_y *= aspect_ratio;
 
-        // Find the pixel position in the world
+        // Find the pixel base offset ralative to the screen center
         var pixel_offset_side = camera_side.scale(pixel_screen_x);
         var pixel_offset_up = camera.up.scale(pixel_screen_y);
         var pixel_offset = pixel_offset_side.add(pixel_offset_up);
-        var pixel_position = screen_center.add(pixel_offset);
 
-        // Cast ray
-        var hit_dist = math.inf(f32);
-        var hit_material: Material = undefined;
-        var ray = pixel_position.sub(camera.position).normalize();
+        // Cast many rays for one pixel (aka anti-aliasing)
+        var final_pixel_color = Vec3.init(0.0, 0.0, 0.0);
+        var i_ray: usize = 0;
+        while (i_ray < n_rays_per_pixel) : (i_ray += 1) {
+            var pixel_position = screen_center.add(pixel_offset);
 
-        // Try intersect ray with planes
-        for (world.planes) |plane| {
-            const denom = plane.normal.dot(ray);
-            if (math.fabs(denom) > EPS) {
-                const normal = plane.normal.normalize();
-                const numer = (-normal.dot(camera.position) + plane.d);
-                const t = numer / denom;
-                if (t > 0.0 and t < hit_dist) {
-                    hit_dist = t;
-                    hit_material = plane.material;
+            // Result color accumulator
+            var attenuation: Vec3 = Vec3.init(1.0, 1.0, 1.0);
+            var pixel_color: Vec3 = Vec3.init(0.0, 0.0, 0.0);
+
+            // Cast ray
+            var ray_origin = camera.position;
+            var ray = pixel_position.sub(camera.position).normalize();
+
+            var i_bounce: usize = 0;
+            while (i_bounce < max_n_ray_bounces) : (i_bounce += 1) {
+                var hit_dist = math.inf(f32);
+                var hit_position: Vec3 = undefined;
+                var hit_normal: Vec3 = undefined;
+                var hit_material: Material = sky_material;
+
+                // Try intersect ray with planes
+                for (world.planes) |plane| {
+                    const denom = plane.normal.dot(ray);
+                    if (math.fabs(denom) > EPS) {
+                        const normal = plane.normal.normalize();
+                        const numer = (-normal.dot(ray_origin) - plane.d);
+                        const t = numer / denom;
+                        if (t > 0.0 and t < hit_dist) {
+                            hit_dist = t;
+                            hit_position = ray_origin.add(ray.scale(t));
+                            hit_material = plane.material;
+                            hit_normal = plane.normal;
+                        }
+                    }
+                }
+
+                // Try intersect ray with spheres
+                for (world.spheres) |sphere| {
+                    const position = ray_origin.sub(sphere.position);
+                    const cos = ray.dot(position);
+                    var d = cos * cos - position.dot(position) + sphere.radius * sphere.radius;
+                    if (d > -EPS) {
+                        d = @max(d, 0);
+                    }
+
+                    if (d >= 0) {
+                        const t = @min(-cos + @sqrt(d), -cos - @sqrt(d));
+                        if (t > 0.0 and t < hit_dist) {
+                            hit_dist = t;
+                            hit_position = ray_origin.add(ray.scale(t));
+                            hit_material = sphere.material;
+                            hit_normal = hit_position.sub(sphere.position).normalize();
+                        }
+                    }
+                }
+
+                // Aufheben hit material color
+                pixel_color = pixel_color.add(
+                    attenuation.mult(hit_material.emission),
+                );
+                attenuation = attenuation.mult(hit_material.albedo);
+                if (hit_dist != math.inf(f32)) {
+                    ray_origin = hit_position;
+                    ray = ray.reflect(hit_normal, 1.0 - hit_material.specular);
+                } else {
+                    break;
                 }
             }
+
+            final_pixel_color = final_pixel_color.add(pixel_color.scale(1.0 / @intToFloat(f32, n_rays_per_pixel)));
         }
 
-        // Try intersect ray with spheres
-        for (world.spheres) |sphere| {
-            const position = camera.position.sub(sphere.position);
-            const cos = ray.dot(position);
-            var d = cos * cos - position.dot(position) + sphere.radius * sphere.radius;
-            if (d > -EPS) {
-                d = @max(d, 0);
-            }
-
-            if (d >= 0) {
-                const t = @min(-cos + @sqrt(d), -cos - @sqrt(d));
-                if (t > 0.0 and t < hit_dist) {
-                    hit_dist = t;
-                    hit_material = sphere.material;
-                }
-            }
-        }
-
-        // TODO: For now it's just a plain albedo color for debug
-        if (hit_dist != math.inf(f32)) {
-            screen.buffer[i_pixel * 3 + 0] = hit_material.albedo.x;
-            screen.buffer[i_pixel * 3 + 1] = hit_material.albedo.y;
-            screen.buffer[i_pixel * 3 + 2] = hit_material.albedo.z;
-        } else {
-            screen.buffer[i_pixel * 3 + 0] = 0.3;
-            screen.buffer[i_pixel * 3 + 1] = 0.3;
-            screen.buffer[i_pixel * 3 + 2] = 0.9;
-        }
+        // Write pixel color to the screen buffer
+        screen.buffer[i_pixel * 3 + 0] = final_pixel_color.x;
+        screen.buffer[i_pixel * 3 + 1] = final_pixel_color.y;
+        screen.buffer[i_pixel * 3 + 2] = final_pixel_color.z;
     }
 }
 
@@ -160,7 +197,7 @@ pub fn main() !void {
         .height = SCREEN_HEIGHT,
     };
     const camera: Camera = Camera{
-        .fov = 90.0 * math.pi / 180.0,
+        .fov = 120.0 * math.pi / 180.0,
         .position = CAMERA_POSITION,
         .forward = CAMERA_FORWARD,
     };
@@ -169,24 +206,54 @@ pub fn main() !void {
         Plane{
             .material = Material{ .albedo = Vec3.init(0.9, 0.8, 0.7) },
             .normal = Vec3.init(0.0, 1.0, 0.0),
-            .d = -5.0,
+            .d = 0.0,
         },
         Plane{
-            .material = Material{ .albedo = Vec3.init(0.7, 0.8, 0.9) },
+            .material = Material{ .albedo = Vec3.init(0.7, 0.8, 0.9), .specular = 0.99 },
+            .normal = Vec3.init(0.0, -1.0, 0.0),
+            .d = -10.0,
+        },
+        Plane{
+            .material = Material{ .albedo = Vec3.init(0.7, 0.8, 0.9), .specular = 0.5 },
+            .normal = Vec3.init(0.0, 0.0, -1.0),
+            .d = -10.0,
+        },
+        Plane{
+            .material = Material{ .albedo = Vec3.init(0.7, 0.8, 0.9), .specular = 0.5 },
+            .normal = Vec3.init(0.0, 0.0, 1.0),
+            .d = -10.0,
+        },
+        Plane{
+            .material = Material{ .albedo = Vec3.init(0.7, 0.8, 0.9), .specular = 0.5 },
+            .normal = Vec3.init(-1.0, 0.0, 0.0),
+            .d = -10.0,
+        },
+        Plane{
+            .material = Material{ .albedo = Vec3.init(0.7, 0.8, 0.9), .specular = 0.5 },
             .normal = Vec3.init(1.0, 0.0, 0.0),
-            .d = -5.0,
+            .d = -10.0,
         },
     };
     const spheres = [_]Sphere{
         Sphere{
-            .material = Material{ .albedo = Vec3.init(0.6, 0.8, 0.5) },
-            .position = Vec3.init(0.0, 0.0, 0.0),
+            .material = Material{ .albedo = Vec3.init(1.0, 0.8, 0.0), .specular = 0.2 },
+            .position = Vec3.init(3.0, 0.0, -2.0),
             .radius = 1.0,
         },
         Sphere{
-            .material = Material{ .albedo = Vec3.init(0.8, 0.6, 0.5) },
-            .position = Vec3.init(2.0, 0.0, 0.0),
+            .material = Material{ .albedo = Vec3.init(0.2, 1.0, 0.2), .specular = 0.2 },
+            .position = Vec3.init(-2.0, 2.0, -1.0),
             .radius = 1.0,
+        },
+        Sphere{
+            .material = Material{ .albedo = Vec3.init(0.4, 0.8, 0.9), .specular = 0.2 },
+            .position = Vec3.init(1.0, 3.0, -1.0),
+            .radius = 1.0,
+        },
+        Sphere{
+            .material = Material{ .emission = Vec3.init(6.0, 6.0, 6.0) },
+            .position = Vec3.init(10.0, 15.0, -6.0),
+            .radius = 2.0,
         },
     };
 
